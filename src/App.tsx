5eb669a0457aefeb149864e0f50af9a1d77b598c
eddiebee <edbeeotudor@gmail.com>
2022-05-03 13:32:35 +0100

    initial setup and build of App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react"; // strictly speaking this is the right way to import React
+import { AppContainer } from "./styles";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// the best practice is to define styles in a separate constant
+const buttonStyles: React.CSSProperties = {
+  backgroundColor: "#5aac44",
+  borderRadius: "3px",
+  border: "none",
+  boxShadow: "none",
+};
+
+export const App = () => {
+  return <AppContainer>Columns will go here</AppContainer>;
+};
 
 export default App;
