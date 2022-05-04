7830fdc5640ef9a6edb7c39408bf39a27e5bda7d
eddiebee <edbeeotudor@gmail.com>
2022-05-04 15:43:30 +0100

    Modify Column to accept children

--- src/Column.tsx
+++ src/Column.tsx
@@ -1,13 +1,15 @@
+import { FC, PropsWithChildren } from "react";
 import { ColumnContainer, ColumnTitle } from "./styles";
 
-type ColumnProps = {
-  text: string;
-};
+type ColumnProps = PropsWithChildren<{
+  text: string; // the text prop will be required by default and we want it to stay so
+}>;
 
-export const Column = ({ text }: ColumnProps) => {
+export const Column: FC<ColumnProps> = ({ text, children }) => {
   return (
     <ColumnContainer>
       <ColumnTitle>{text}</ColumnTitle>
+      {children}
     </ColumnContainer>
   );
 };
