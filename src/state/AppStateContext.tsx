46cf5ebab94e0a03128e5209a6322dce93a9fb01
eddiebee <edbeeotudor@gmail.com>
2022-05-06 18:35:13 +0100

    Implement state management

---
+++ src/state/AppStateContext.tsx
@@ -0,0 +1,65 @@
+import { createContext, useContext, FC, PropsWithChildren } from "react";
+
+// we need to provide the type for our context.
+type AppStateContextProps = PropsWithChildren<{
+  lists: List[];
+  getTasksByListId(id: string): Task[];
+}>;
+
+const AppStateContext = createContext<AppStateContextProps>(
+  {} as AppStateContextProps
+);
+
+type Task = {
+  id: string;
+  text: string;
+};
+
+type List = {
+  id: string;
+  text: string;
+  tasks: Task[];
+};
+
+export type AppState = {
+  lists: List[];
+};
+
+const appData: AppState = {
+  lists: [
+    {
+      id: "0",
+      text: "To Do",
+      tasks: [{ id: "c0", text: "Generate app scaffold" }],
+    },
+    {
+      id: "1",
+      text: "In Progress",
+      tasks: [{ id: "c2", text: "Learn Typescript" }],
+    },
+    {
+      id: "2",
+      text: "Done",
+      tasks: [{ id: "c3", text: "Begin to use static typing" }],
+    },
+  ],
+};
+
+export const AppStateProvider: FC<AppStateContextProps> = ({ children }) => {
+  const { lists } = appData;
+
+  const getTasksByListId = (id: string) => {
+    return lists.find((list) => list.id === id)?.tasks || [];
+  };
+
+  return (
+    <AppStateContext.Provider value={{ lists, getTasksByListId }}>
+      {children}
+    </AppStateContext.Provider>
+  );
+};
+
+// custom hook to use data from Global context
+export const useAppState = () => {
+  return useContext(AppStateContext);
+};
