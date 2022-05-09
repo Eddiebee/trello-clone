7abed15ecfa7883df7c83281da7efe91abc5b3e9
eddiebee <edbeeotudor@gmail.com>
2022-05-09 14:19:29 +0100

    setup drag and drop feature

--- src/state/AppStateContext.tsx
+++ src/state/AppStateContext.tsx
@@ -2,9 +2,11 @@ import { createContext, useContext, Dispatch, FC } from "react";
 import { Action } from "./actions";
 import { appStateReducer, AppState, List, Task } from "./appStateReducer";
 import { useImmerReducer } from "use-immer";
+import { DragItem } from "../DragItem";
 
 // we need to provide the type for our context.
 type AppStateContextProps = {
+  draggedItem: DragItem | null;
   lists: List[];
   getTasksByListId(id: string): Task[];
   dispatch: Dispatch<Action>;
@@ -15,6 +17,7 @@ const AppStateContext = createContext<AppStateContextProps>(
 );
 
 const appData: AppState = {
+  draggedItem: null,
   lists: [
     {
       id: "0",
@@ -36,14 +39,16 @@ const appData: AppState = {
 
 export const AppStateProvider = ({ children }: any) => {
   const [state, dispatch] = useImmerReducer(appStateReducer, appData);
-  const { lists } = state;
+  const { draggedItem, lists } = state;
 
   const getTasksByListId = (id: string) => {
     return lists.find((list) => list.id === id)?.tasks || [];
   };
 
   return (
-    <AppStateContext.Provider value={{ lists, getTasksByListId, dispatch }}>
+    <AppStateContext.Provider
+      value={{ draggedItem, lists, getTasksByListId, dispatch }}
+    >
       {children}
     </AppStateContext.Provider>
   );
